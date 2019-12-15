67955d70515bbfb539b2d945407935f50a5aa0e5
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-12-15 00:41:08 +0000

    WIP: Trying to get barebones tests going

--- tests/Events/EventRegistrationEmbargoTest.php
+++ tests/Events/EventRegistrationEmbargoTest.php
@@ -16,38 +16,6 @@ class EventRegistrationEmbargoTest extends SapphireTest {
 
     use DateTimeHelperTrait;
 
-    /*
-     * | Field                    | Type                                     | Null | Key | Default                       | Extra          |
-+--------------------------+------------------------------------------+------+-----+-------------------------------+----------------+
-| ID                       | int(11)                                  | NO   | PRI | NULL                          | auto_increment |
-| ClassName                | enum('TitleDK\\Calendar\\Events\\Event') | YES  | MUL | TitleDK\Calendar\Events\Event |                |
-| LastEdited               | datetime                                 | YES  | MUL | NULL                          |                |
-| Created                  | datetime                                 | YES  |     | NULL                          |                |
-| Title                    | varchar(200)                             | YES  |     | NULL                          |                |
-| AllDay                   | tinyint(1) unsigned                      | NO   |     | 0                             |                |
-| NoEnd                    | tinyint(1) unsigned                      | NO   |     | 0                             |                |
-| StartDateTime            | datetime                                 | YES  | MUL | NULL                          |                |
-| TimeFrameType            | enum('Duration','DateTime')              | YES  |     | Duration                      |                |
-| Duration                 | time                                     | YES  |     | NULL                          |                |
-| EndDateTime              | datetime                                 | YES  |     | NULL                          |                |
-| Details                  | mediumtext                               | YES  |     | NULL                          |                |
-| EventPageID              | int(11)                                  | NO   | MUL | 0                             |                |
-| CalendarID               | int(11)                                  | NO   | MUL | 0                             |                |
-| Registerable             | tinyint(1) unsigned                      | NO   |     | 0                             |                |
-| TicketsRequired          | tinyint(1) unsigned                      | NO   |     | 0                             |                |
-| PaymentRequired          | tinyint(1) unsigned                      | NO   |     | 0                             |                |
-| RSVPEmail                | varchar(255)                             | YES  |     | NULL                          |                |
-| CostCurrency             | varchar(3)                               | YES  |     | NULL                          |                |
-| CostAmount               | decimal(19,4)                            | NO   |     | 0.0000                        |                |
-| FeaturedImageID          | int(11)                                  | NO   | MUL | 0                             |                |
-| URLSegment               | varchar(255)                             | YES  |     | NULL                          |                |
-| MemberCostCurrency       | varchar(3)                               | YES  |     | NULL                          |                |
-| MemberCostAmount         | decimal(19,4)                            | NO   |     | 0.0000                        |                |
-| LocationName             | varchar(255)                             | YES  |     | NULL                          |                |
-| MapURL                   | varchar(255)                             | YES  |     | NULL                          |                |
-| NumberOfAvailableTickets | int(11)                                  | NO   |     | 0                             |                |
-| RegistrationEmbargoAt    | datetime                                 | YES  |     | NULL
-     */
 
     public function setUp()
     {
