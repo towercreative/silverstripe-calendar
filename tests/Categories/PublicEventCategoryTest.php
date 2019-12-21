8a09fe1da789c1e0a7687de900b3f1c55b41ea00
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-12-21 07:29:29 +0000

    WIP: Having issues with fixtures, committing a working copy

--- tests/Categories/PublicEventCategoryTest.php
+++ tests/Categories/PublicEventCategoryTest.php
@@ -3,17 +3,48 @@
 namespace TitleDK\Calendar\Tests\Categories;
 
 use \SilverStripe\Dev\SapphireTest;
+use TitleDK\Calendar\Categories\PublicEventCategory;
 
 class PublicEventCategoryTest extends SapphireTest
 {
+    protected static $fixture_file = 'tests/events.yml';
+
+    /** @var PublicEventCategory */
+    private $category1;
+
+    /** @var PublicEventCategory */
+    private $category2;
+
+    public function setUp()
+    {
+        // this has to happen before accessing the fixtures
+        parent::setUp();
+
+        $this->category1 = $this->objFromFixture('TitleDK\Calendar\Categories\PublicEventCategory', 'category1');
+        $this->category2 = $this->objFromFixture('TitleDK\Calendar\Categories\PublicEventCategory', 'category2');
+    }
+
     public function testComingEvents()
     {
-        $this->markTestSkipped('TODO');
+        $category1 = $this->objFromFixture('TitleDK\Calendar\Categories\PublicEventCategory', 'category1');
+
+        /** @var PublicEventCategory $category1 */
+        $events = $this->category1->comingEvents()->toArray();
+        $titles = array_map(function($event) {
+            return $event->Title;
+        }, $events);
+
+        $this->assertEquals([], $titles);
+
+        foreach($events as $event) {
+            error_log($event->Title);
+        }
+
     }
 
     public function testCanView()
     {
-        $this->markTestSkipped('TODO');
+        $this->assertTrue($this->category1->canView());
     }
 
     public function testCanCreate()
