64b938ae86d5e6f1f44026610b80e9cdb530103d
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-12-21 18:17:35 +0000

    MINOR: Remove obvious test entity

--- src/Calendars/GroupsCalendarExtension.php
+++ src/Calendars/GroupsCalendarExtension.php
@@ -4,10 +4,13 @@ namespace TitleDK\Calendar\Calendars;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\ORM\DataList;
 
 /**
  * Event Calendar Extension
  * Allowing events to have calendars
+ *
+ * @method DataList Calendars Get the calendars associated with the group
  *
  * @package calendar
  * @subpackage calendars
@@ -15,7 +18,6 @@ use SilverStripe\ORM\DataExtension;
 class GroupsCalendarExtension extends DataExtension
 {
     private static $many_many = array(
-        'Calendar' => 'TitleDK\Calendar\Calendars\Calendar',
-        'Wibble' => 'Varchar'
+        'Calendar' => 'TitleDK\Calendar\Calendars\Calendar'
     );
 }
