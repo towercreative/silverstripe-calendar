323512bb550b4bfe91b29ab4e0bdf44b9a0544a3
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-01-01 18:38:19 +0000

    ENHANCEMENT: Trying to unbloat CalendarPageController, this is the search method moved to a helper

--- src/Helpers/CalendarPageHelper.php
+++ src/Helpers/CalendarPageHelper.php
@@ -18,5 +18,32 @@ use TitleDK\Calendar\Registrations\EventRegistration;
 
 class CalendarPageHelper
 {
+    public function performSearch($query)
+    {
+        // @todo This is case sensitive with Postgresql, not so with MySQL
+        //$query = strlower(addslashes($query));
+        $query = (addslashes($query));
+        //Debug::dump($query);
+        $qarr = preg_split('/[ +]/', $query);
 
+        $filter = '';
+        $first = true;
+        foreach ($qarr as $qitem) {
+            if (!$first) {
+                $filter .= " AND ";
+            }
+
+            $filter .= " (
+					\"Title\" LIKE '%$qitem%'
+					OR \"Details\" LIKE '%$qitem%'
+				)";
+            $first = false;
+        }
+
+
+        //Debug::dump($filter);
+        $events = CalendarHelper::all_events()
+            ->where($filter);
+        return $events;
+    }
 }
