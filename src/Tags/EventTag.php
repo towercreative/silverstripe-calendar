b01b53e4bbcc68e33ddbe96f99e6b4d7016cd145
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-01-04 00:02:49 +0000

    WIP: Migration from DOURLSegmentExtension to sluggable

--- src/Tags/EventTag.php
+++ src/Tags/EventTag.php
@@ -11,7 +11,7 @@ use TitleDK\Calendar\Events\Event;
  * A blog tag for keyword descriptions of a blog post.
  *
  * @property string $Title
- * @property string $URLSegment
+ * @property string $Slug
  * @method \SilverStripe\ORM\ManyManyList|\TitleDK\Calendar\Events\Event[] Events()
  */
 class EventTag extends DataObject
@@ -38,8 +38,7 @@ class EventTag extends DataObject
      * @var array
      */
     private static $db = [
-        'Title'      => 'Varchar(255)',
-        'URLSegment' => 'Varchar(255)'
+        'Title'      => 'Varchar(255)'
     ];
 
     /**
@@ -75,19 +74,6 @@ class EventTag extends DataObject
         return _t(__CLASS__ . '.Duplicate', 'A blog tag already exists with that name.');
     }
 
-    // ---- slug related, todo find a module ----
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function onBeforeWrite()
-    {
-        parent::onBeforeWrite();
-        if ($this->exists() || empty($this->URLSegment)) {
-            return $this->generateURLSegment();
-        }
-    }
-
 
     /**
      * Looks for objects o the same type and the same value by the given Field
@@ -100,8 +86,7 @@ class EventTag extends DataObject
         $duplicates = DataList::create(self::class)
             ->filter(
                 [
-                    $field   => $this->$field,
-                  //  'BlogID' => (int) $this->BlogID
+                    $field   => $this->$field
                 ]
             );
 
@@ -112,35 +97,4 @@ class EventTag extends DataObject
         return $duplicates;
     }
 
-
-    /**
-     * Generates a unique URLSegment from the title.
-     *
-     * @param int $increment
-     *
-     * @return string
-     */
-    public function generateURLSegment($increment = 0)
-    {
-        $increment = (int) $increment;
-        $filter = URLSegmentFilter::create();
-
-        // Setting this to on. Because of the UI flow, it would be quite a lot of work
-        // to support turning this off. (ie. the add by title flow would not work).
-        // If this becomes a problem we can approach it then.
-        // @see https://github.com/silverstripe/silverstripe-blog/issues/376
-        $filter->setAllowMultibyte(true);
-
-        $this->URLSegment = $filter->filter($this->Title);
-
-        if ($increment > 0) {
-            $this->URLSegment .= '-' . $increment;
-        }
-
-        if ($this->getDuplicatesByField('URLSegment')->count() > 0) {
-            $this->generateURLSegment($increment + 1);
-        }
-
-        return $this->URLSegment;
-    }
 }
