b40be3439c29fc7f9d44d012d7df87428adf6ed5
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-01-04 23:24:47 +0000

    ENHANCEMENT: Eeked out the last line of Calendar.php by the addition of Group

--- tests/Calendars/CalendarTest.php
+++ tests/Calendars/CalendarTest.php
@@ -3,17 +3,47 @@
 namespace TitleDK\Calendar\Tests\Calendars;
 
 use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Security\Member;
 use TitleDK\Calendar\Calendars\Calendar;
 
 class CalendarTest extends SapphireTest
 {
+    protected static $fixture_file = ['tests/events.yml'];
+
     /** @var Calendar */
     private $calendar;
 
+    /** @var Member */
+    private $member;
+
+
+
     public function setUp()
     {
-        $this->calendar = new Calendar();
-        return parent::setUp();
+        parent::setUp();
+
+        $this->calendar = $this->objFromFixture(Calendar::class, 'testCalendar1');
+        $this->member = $this->objFromFixture(Member::class, 'member1');
+        $this->logInAs($this->member);
+    }
+
+    public function test_get_cms_fields()
+    {
+        // this will contain the root tab
+        $fields = $this->calendar->getCMSFields();
+
+        /** @var TabSet $rootTab */
+        $rootTab = $fields->fieldByName('Root');
+
+        /** @var Tab $mainTab */
+        $mainTab = $rootTab->fieldByName('Main');
+        $fields = $mainTab->FieldList()->toArray();
+
+        $titles = array_map(function($field) {
+            return $field->Name;
+        }, $fields);
+
+        $this->assertEquals(['Slug', 'Title', 'Color', 'Groups'], $titles);
     }
 
 
